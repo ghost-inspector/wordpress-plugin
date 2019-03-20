a42d4cf62ef92c96ddcf6a9e3579b4e6221c97ab
Jordan Kohl <kohl.jordan@gmail.com>
2019-03-20 13:51:39 -0700

    organizes PHP and only loads JS on dashboard

--- ghost-inspector.php
+++ ghost-inspector.php
@@ -3,21 +3,34 @@
  * Plugin Name: Ghost Inspector
  */
 
-if (!in_array($_SERVER['REMOTE_ADDR'], array('10.255.0.2', '::1'))) {
-  $JSfiles = scandir(dirname(__FILE__) . '/frontend/build/static/js/');
-  $react_js_to_load = '';
-   foreach($JSfiles as $filename) {
-     if(strpos($filename,'.js')&&!strpos($filename,'.js.map')) {
-       $react_js_to_load = plugin_dir_url( __FILE__ ) . 'frontend/build/static/js/' . $filename;
-     }
-   }
-} else {
-  $react_js_to_load = 'http://localhost:3000/static/js/bundle.js';
-}
-// DEV React dynamic loading
-wp_enqueue_script('ghost_inspector_react', $react_js_to_load, '', mt_rand(10,1000), true);
+function gi_load_scripts ($hook) {
+  // only load scripts on dashboard
+  if ($hook != 'index.php') {
+    return;
+  }
 
-add_action('wp_ajax_gi_api_proxy', 'gi_api_proxy' );
+  if (!in_array($_SERVER['REMOTE_ADDR'], array('10.255.0.2', '::1'))) {
+    $JSfiles = scandir(dirname(__FILE__) . '/frontend/build/static/js/');
+    $react_js_to_load = '';
+    foreach($JSfiles as $filename) {
+      if(strpos($filename,'.js')&&!strpos($filename,'.js.map')) {
+        $react_js_to_load = plugin_dir_url( __FILE__ ) . 'frontend/build/static/js/' . $filename;
+      }
+    }
+  } else {
+    // DEV React dynamic loading
+    $react_js_to_load = 'http://localhost:3000/static/js/bundle.js';
+  }
+  wp_enqueue_script('ghost_inspector_react', $react_js_to_load, '', mt_rand(10,1000), true);
+  $gi_suite_id = '5be210847a05a37dcf89fc43'; // TODO: get from WP plugin settings
+  $gi_title_nonce = wp_create_nonce('gi_api_proxy');
+  wp_localize_script('ghost_inspector_react', 'gi_ajax', array(
+    'ajax_url' => admin_url('admin-ajax.php'),
+    'nonce'    => $gi_title_nonce,
+    'suiteId'  => $gi_suite_id,
+  ));
+}
+
 function gi_api_proxy() {
     // Handle the ajax request
     check_ajax_referer('gi_api_proxy');
@@ -33,20 +46,15 @@ function gi_api_proxy() {
     wp_die(); // All ajax handlers die when finished
 }
 
-add_action('wp_dashboard_setup', 'gi_add_widget');
-  
 function gi_add_widget() {
-  global $wp_meta_boxes;
-  $gi_suite_id = '5be210847a05a37dcf89fc43'; // TODO: get from WP plugin settings
-  $gi_title_nonce = wp_create_nonce('gi_api_proxy');
-  wp_localize_script('ghost_inspector_react', 'gi_ajax', array(
-    'ajax_url' => admin_url('admin-ajax.php'),
-    'nonce'    => $gi_title_nonce,
-    'suiteId'  => $gi_suite_id,
-  ));
+  // global $wp_meta_boxes;
   wp_add_dashboard_widget('ghost_inspector_widget', 'Ghost Inspector', 'gi_display_widget');
 }
- 
+
 function gi_display_widget() {
   include 'ghost-inspector-display.php';
 }
+
+add_action('admin_enqueue_scripts', 'gi_load_scripts');
+add_action('wp_ajax_gi_api_proxy', 'gi_api_proxy' );
+add_action('wp_dashboard_setup', 'gi_add_widget');
