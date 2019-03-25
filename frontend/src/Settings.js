c952d95bd1ecf97b25570cc21bab7cddbac67c1c
Jordan Kohl <kohl.jordan@gmail.com>
2019-03-25 10:54:04 -0700

    roughs in settings UI

--- frontend/src/Settings.js
+++ frontend/src/Settings.js
@@ -1,7 +1,21 @@
 import React, { useEffect, useState }  from 'react';
 
-const Settings = () => (
-  <div>Settings  go here</div>
-);
+const Settings = () => {
+  const [apiKey, setApiKey] = useState('')
+  const [suiteId, setSuiteId] = useState('')
+  const updateApiKey = (event) => setApiKey(event.target.value)
+  const updateSuiteId = (event) => setSuiteId(event.target.value)
+  const updateSettings = (event) => {
+    event.preventDefault()
+    console.log(apiKey, suiteId)
+  }
+  return (
+    <form onSubmit={updateSettings}>
+      <p><label>API Key: <input type="text" value={apiKey} onChange={updateApiKey} /></label></p>
+      <p><label>Suite ID: <input type="text" value={suiteId} onChange={updateSuiteId} /></label></p>
+      <p><button type="submit" className="button button-primary">Submit</button></p>
+    </form>
+  );
+}
 
 export default Settings;
