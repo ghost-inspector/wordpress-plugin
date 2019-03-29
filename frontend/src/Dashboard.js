53a32612576cc58252af8fbfe28ea92a6dedbf21
Jordan Kohl <kohl.jordan@gmail.com>
2019-03-29 16:07:32 -0700

    table styling and export path (#10)
    * adds table styling for dashboard test list
    
    * switches from list to table markup with headers
    
    * changes export to put files into ghost-inspector folder

--- frontend/src/Dashboard.js
+++ frontend/src/Dashboard.js
@@ -55,13 +55,26 @@ const Dashboard = ({ suiteId, executeEnabled }) => {
   return (
     <div className="ghost_inspector_wrapper">
       <p className="ghost_inspector_header">Latest results for suite: <a href={`${baseUrl}/suites/${suiteId}`} target="_blank" rel="noopener noreferrer" className="ghost_inspector_suite_name">{suite.name}</a> ({totalPassing}/{total} passing)</p>
-      <ul className="ghost_inspector_tests">
-        {tests.map(test => (
-          <li key={test._id}>
-            <a href={`${baseUrl}/tests/${test._id}`} target="_blank" rel="noopener noreferrer">{test.name}</a> <span className={`ghost_inspector_${test.passing ? 'passing' : 'failing'}`}>{test.passing ? 'passed' : 'failed'}</span> on {format(new Date(test.dateExecutionFinished), 'MMM D, YYYY @ h:mm:ss A')}
-          </li>
-        ))}
-      </ul>
+      <div className="ghost_inspector_tests">
+        <table>
+          <tr>
+            <th>Test Name</th>
+            <th>Last Run</th>
+          </tr>
+          {tests.map(test => (
+            <tr key={test._id}>
+              <td>
+                <a href={`${baseUrl}/tests/${test._id}`} target="_blank" rel="noopener noreferrer">{test.name}</a>
+              </td>
+              <td className="ghost_inspector_status">
+                <span className={`dashicons dashicons-${test.passing ? 'yes' : 'no'}`}></span>
+                {format(new Date(test.dateExecutionFinished), 'MMM D')}
+              </td>
+            </tr>
+          ))}
+        </table>
+      </div>
+
       {executeEnabled && <p><button type="button" className="button button-primary" onClick={triggerExecuteSuite} disabled={isSuiteRunning}>Run Test Suite</button></p>}
     </div>
   );
