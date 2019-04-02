06197d00cc62ccf01063c95a66e2964b9bd56881
Jordan Kohl <kohl.jordan@gmail.com>
2019-04-02 12:35:19 -0700

    adds GPLv3 license [ch1444]

--- ghost-inspector.php
+++ ghost-inspector.php
@@ -7,7 +7,10 @@
  * Author: Ghost Inspector
  * Author URI: https://ghostinspector.com
  * Text Domain: ghost-inspector
- */
+ * License: GPLv3
+ *
+ * Ghost Inspector is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 2 of the License, or any later version. Ghost Inspector is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with Ghost Inspector. If not, see https://www.gnu.org/licenses/gpl-3.0.txt.
+*/
 
 // proxy requests to the GI API so that the API key remains hidden
 function gi_api_proxy($request) {
--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: simpixelated
 Tags: testing
 Requires at least: 3.0.1
 Tested up to: 5.1
-Requires PHP: 5.2.4
+Requires PHP: 5.4
 Stable tag: 1.0.0
 
 Display the latest test results of a single suite on your dashboard. To get started: sign up for an account on ghostinspector.com and add your API key and your suite ID to your Ghost Inspector settings here in Wordpress. Build or record automated website tests in your browser with Ghost Inspector. We run them continuously from the cloud, monitoring your website for issues.
